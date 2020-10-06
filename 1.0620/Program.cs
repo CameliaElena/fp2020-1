04b3e84f04bfe385415910421ba5545df01f69bf
CameliaElena <calugareanu.camelia@gmail.com>
2020-10-06 12:18:41 -0700

    add code to compute distance to stormwind

--- 1.0620/Program.cs
+++ 1.0620/Program.cs
@@ -1,10 +1,29 @@
 ﻿
+using System;
+/// <summary>
+/// distanta pana la stormwind
+/// </summary>
+/// // XML
 namespace _1._0620
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int k;
+            Console.WriteLine("introduceti distanta la care suntenti pe autostrada");
+            string line;
+            line = Console.ReadLine();
+
+            k = int.Parse(line);
+            int d;
+            if (k > 60)
+                d = k - 60;
+            else
+                d = 60 - k;
+            Console.WriteLine("va aflati la {0} km de stormwind", d );
+            //Console.WriteLine($"va aflati la{d} km de stormwind");
+
         }
     }
 }
